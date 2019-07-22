09338277bd140019e8c6b63566699c68addba870
ncarlson01 <53132305+ncarlson01@users.noreply.github.com>
2019-07-22 02:04:46 -0500

    Update svm.py
    working

--- svm.py
+++ svm.py
@@ -10,9 +10,7 @@ import pandas as pd
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.utils import shuffle
-
-import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 
 #designate input file
 input_file = "chatdata.csv"
@@ -22,30 +20,33 @@ dataset = pd.read_csv(input_file, header = 0,  sep=',')
 
 #select data
 X = dataset.iloc[:, 2:]  #select columns 2 through end, predictors
-Y = dataset.iloc[:, 1]   #select column 1, target
+y = dataset.iloc[:, 1]   #select column 1, target
 
 #shuffle the data and split the sample into training and test data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, Y, train_size=433, test_size=100, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split( X, y, train_size=433, test_size=100, shuffle=True)
 
 #standarize features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
-clf.fit(X, Y) 
 clf = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
     decision_function_shape='ovo', degree=3, gamma='scale', kernel='rbf',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
-dec = clf.decision_function([[1]])
-dec.shape[1] # 4 classes: 4*3/2 = 6
 
 clf.fit(X_train, y_train)
-sparsity = np.mean(clf.coef_ == 0) * 100
+
+#sparsity = np.mean(clf.coef_ == 0) * 100
 score = clf.score(X_test, y_test)
 
 # print('Best C % .4f' % clf.C_)
 print("Sparsity with L1 penalty: %.2f%%" % sparsity)
 print("Test score with L1 penalty: %.4f" % score)
+
+#dec = clf.decision_function([[1]])
+#dec.shape[126] # 4 classes: 4*3/2 = 6
+
+#clf.decision_function_shape = "ovr"
+#dec = clf.decision_function([[1]])
+#dec.shape[126] # 4 classes
