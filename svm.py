8e57751151e0206e71835d9c4fc22b8580810f8e
ncarlson01 <53132305+ncarlson01@users.noreply.github.com>
2019-07-23 04:16:42 -0500

    Update svm.py
    added classification report

--- svm.py
+++ svm.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-@author: Nick
-"""
-
-import numpy as np
 import pandas as pd
-from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.metrics import classification_report
 
 #designate input file
 input_file = "chatdata.csv"
@@ -35,16 +29,12 @@ clf = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
 
 clf.fit(X_train, y_train)
 
-#sparsity = np.mean(clf.coef_ == 0) * 100
+#accuracy
 score = clf.score(X_test, y_test)
 
-# print('Best C % .4f' % clf.C_)
-#print("Sparsity with L1 penalty: %.2f%%" % sparsity)
+#print classification report
+clf = clf.predict(X_test)
+report = classification_report(y_test, clf)
+print(report)
+
 print("Test score with L1 penalty: %.4f" % score)
-
-#dec = clf.decision_function([[1]])
-#dec.shape[126] # 4 classes: 4*3/2 = 6
-
-#clf.decision_function_shape = "ovr"
-#dec = clf.decision_function([[1]])
-#dec.shape[126] # 4 classes
