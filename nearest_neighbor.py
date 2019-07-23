8859c7f089a955218d2a3d8d8afbf6625c4e8898
ncarlson01 <53132305+ncarlson01@users.noreply.github.com>
2019-07-23 04:18:00 -0500

    Update nearest_neighbor.py
    working now. complete with classification report.

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,33 +1,45 @@
-#wip
-
-import numpy as np
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.utils import shuffle
-import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 #designate input file
 input_file = "chatdata.csv"
 
 #pandas read input csv
-df = pd.read_csv(input_file, header = 0)
+dataset = pd.read_csv(input_file, header = 0,  sep=',')
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
-distances, indices = nbrs.kneighbors(X)
-indices                                           
-array([[0, 1],
-       [1, 0],
-       [2, 1],
-       [3, 4],
-       [4, 3],
-       [5, 4]]...)
-distances
-array([[0.        , 1.        ],
-       [0.        , 1.        ],
-       [0.        , 1.41421356],
-       [0.        , 1.        ],
-       [0.        , 1.        ],
-       [0.        , 1.41421356]])
+#select data
+X = dataset.iloc[:, 2:]  #select columns 2 through end, predictors
+y = dataset.iloc[:, 1]   #select column 1, target
+
+#shuffle the data and split the sample into training and test data
+X_train, X_test, y_train, y_test = train_test_split( X, y, train_size=433, test_size=100, shuffle=True)
+
+#standarize features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+#kneighbors classifier object
+knn = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, 
+                           p=2, metric='minkowski', metric_params=None, n_jobs=None)
+
+#fit model
+knn.fit(X_train, y_train)
+
+#response prediction
+pred = knn.predict(X_test)
+
+#accuracy
+knn.score(X_test, y_test)
+
+#print classification report
+knn = knn.predict(X_test)
+report = classification_report(y_test, knn)
+print(report)
+
+#evaluate accuracy
+print("Accuracy on test set: %0.3f%%"%(accuracy_score(y_test, pred)*100))
