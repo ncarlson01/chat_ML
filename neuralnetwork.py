f0542c8c75d0b06301eaedde41066d9969eea8cc
ncarlson01 <53132305+ncarlson01@users.noreply.github.com>
2019-07-31 02:45:07 -0500

    Update neuralnetwork.py
    updated parameters

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-@author: Nick
-"""
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report
 
 #designate input file
 input_file = "chatdata.csv"
@@ -29,19 +25,21 @@ X_test = scaler.transform(X_test)
 #multinomial logistic regression object using L1 penalty
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 
-MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+MLPClassifier(activation='logistic', alpha=1e-05, batch_size='auto',
               beta_1=0.9, beta_2=0.999, early_stopping=False,
               epsilon=1e-08, hidden_layer_sizes=(5, 2),
-              learning_rate='constant', learning_rate_init=0.001,
               max_iter=200, momentum=0.9, n_iter_no_change=10,
               nesterovs_momentum=True, power_t=0.5, random_state=1,
-              shuffle=True, solver='lbfgs', tol=0.0001,
-              validation_fraction=0.1, verbose=False, warm_start=False)
+              solver='lbfgs', tol=0.0001, validation_fraction=0.1, 
+              verbose=False, warm_start=False)
 
 #train model
 clf.fit(X_train, y_train)
 score = clf.score(X_test, y_test)
 
-# print('Best C % .4f' % clf.C_)
-# print("Sparsity with L1 penalty: %.2f%%" % sparsity)
+#print classification report
+clf = clf.predict(X_test)
+report = classification_report(y_test, clf)
+print(report)
+
 print("Test score with L1 penalty: %.4f" % score)
